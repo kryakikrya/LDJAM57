2b1c465cdb4c76968c1a29120bafa33c9df1c027
kryakikrya <gon4.egor@gmail.com>
2025-04-06 22:37:07 +0300

    Start comix

---
+++ LDJAM/Assets/Scripts/Comix1.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Comix1 : MonoBehaviour
+{
+
+    [SerializeField] List<Sprite> list = new List<Sprite>();
+    private int id = 0;
+    [SerializeField] Image image;
+    private void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            if (id > 0)
+            {
+                id -= 1;
+                image.sprite = list[id];
+            }
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            if (id < list.Count - 1)
+            {
+                id += 1;
+                image.sprite = list[id];
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
